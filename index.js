115d237d02190dc121c8126e4c3bc5d64ec4b2b0
Alejandro Isa <alejandroisa@gmail.com>
2021-03-10 00:28:16 -0300

    creando un master Page en Handlebars

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const router = require("./routes");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("funciona");
-});
+//habilitar handlebars como view
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "layout",
+  })
+);
+app.set("view engine", "handlebars");
+
+//static files
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", router());
 
 app.listen(5000);
