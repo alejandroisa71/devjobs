b0925618ae4c1423049b26553c893a75996903c1
Alejandro Isa <alejandroisa@gmail.com>
2021-03-15 12:33:25 -0300

    configuracion de la BD en la aplicacion (erroren require('connect--mongo')(session)

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const mongoose = require("mongoose");
+require("./config/db");
+
 const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
 const router = require("./routes");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 require("dotenv").config({ path: "variables.env" });
 
@@ -18,6 +24,18 @@ app.set("view engine", "handlebars");
 
 //static files
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
 app.use("/", router());
 
