07c6bfe3b49a9f8ab46f83b8e13f29e5e59c318d
Alejandro Isa <alejandroisa@gmail.com>
2021-03-16 01:25:16 -0300

    creando ruta, controller y formulario para Nuevas Vacantes

---
+++ controllers/vacantesController.js
@@ -0,0 +1,6 @@
+exports.formularioNuevaVacante = (req, res) => {
+  res.render("nueva-vacante", {
+    nombrePagina: "Nueva Vacante",
+    tagline: "Llena el Formulario y publica tu vacante",
+  });
+};
