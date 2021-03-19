82131f9d0de71a1fd894e415dce1d8d48223a21d
Alejandro Isa <alejandroisa71@gmail.com>
2021-03-19 17:45:00 -0300

    mostrando vacantes en la pagina principal (con errores)

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,19 @@
-exports.mostrarTrabajos = (req, res) => {
+const mongoose= require('mongoose');
+const Vacante = mongoose.model('Vacante')
+
+exports.mostrarTrabajos = async (req, res,next) => {
+
+  const vacantes = await Vacante.find();
+
+  if(!vacantes) return next();
+
+ console.log(vacantes) 
+
   res.render("home", {
     nombrePagina: "devJobs",
     tagline: "Encuentra y Publica trabajos para Desarrolladores Web",
     barra: true,
     boton: true,
+    vacantes
   });
 };
