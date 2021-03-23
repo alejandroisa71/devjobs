5d48e407d11bbf6024dc6ae3d5126a5ca0f6cbd0
Alejandro Isa <alejandroisa71@gmail.com>
2021-03-23 19:34:12 -0300

    mostrando Errores en Alertas

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -45,4 +45,18 @@ module.exports = {
       .fn(this)
       .replace(new RegExp(`value="${seleccionado}"`), '$& selected="selected"');
   },
+  mostrarAlertas:(errores ={}, alertas) =>{
+    const categoria = Object.keys(errores);
+
+    let html='';
+    if(categoria.length){
+        errores[categoria].forEach(error =>{
+          html +=`<div class="${categoria} alerta">
+              ${error}
+          </div>  `;
+        })
+    }
+
+    return alertas.fn().html = html;
+  }
 };
