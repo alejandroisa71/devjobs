fc4d10fb6d0ef131359230db6ef62464c2b68c23
Alejandro Isa <alejandroisa@gmail.com>
2021-03-24 14:30:54 -0300

    Validando errores de MongoDb

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,57 +1,62 @@
-const mongoose = require ('mongoose');
-const Usuarios = mongoose.model('Usuarios')
+const mongoose = require("mongoose");
+const Usuarios = mongoose.model("Usuarios");
 
-exports.formCrearCuenta =(req,res)=>{
-   res.render('crear-cuenta',{
-       nombrePagina: 'Crea tu cuenta En devJobs',
-       tagline: 'Comienza a publicar tus vacantes gratis,solo debes crear una cuenta '
-   }) 
-}
+exports.formCrearCuenta = (req, res) => {
+  res.render("crear-cuenta", {
+    nombrePagina: "Crea tu cuenta En devJobs",
+    tagline:
+      "Comienza a publicar tus vacantes gratis,solo debes crear una cuenta ",
+  });
+};
 
-exports.validarRegistro = (req,res,next)=>{
+exports.validarRegistro = (req, res, next) => {
+  //sanitizar los campos
+  req.sanitizeBody("nombre").escape();
+  req.sanitizeBody("email").escape();
+  req.sanitizeBody("password").escape();
+  req.sanitizeBody("confirmar").escape();
 
-    //sanitizar los campos
-    req.sanitizeBody('nombre').escape();
-    req.sanitizeBody('email').escape();
-    req.sanitizeBody('password').escape();
-    req.sanitizeBody('confirmar').escape();
+  //validar campos
+  req.checkBody("nombre", "El Nombre es Obligatorio").notEmpty();
+  req.checkBody("email", "El email debe ser Válido").isEmail();
+  req.checkBody("password", "El Password no puede ir vacio").notEmpty();
+  req.checkBody("confirmar", "Confirmar Password no puede ir vacio").notEmpty();
+  //compara passqoerd y confirmar
+  req
+    .checkBody("confirmar", "El password es diferente")
+    .equals(req.body.password);
 
-    //validar campos
-    req.checkBody('nombre', 'El Nombre es Obligatorio').notEmpty();   
-    req.checkBody('email', 'El email debe ser Válido').isEmail();   
-    req.checkBody('password', 'El Password no puede ir vacio').notEmpty();   
-    req.checkBody('confirmar', 'Confirmar Password no puede ir vacio').notEmpty();   
-    //compara passqoerd y confirmar
-    req.checkBody('confirmar', 'El password es diferente').equals(req.body.password);
+  const errores = req.validationErrors();
 
-    const errores = req.validationErrors();
+  if (errores) {
+    //si hay errores
+    req.flash(
+      "error",
+      errores.map((error) => error.msg)
+    );
 
-    if(errores){
-        //si hay errores
-        req.flash('error',errores.map(error=> error.msg));
+    res.render("crear-cuenta", {
+      nombrePagina: "Crea tu cuenta En devJobs",
+      tagline:
+        "Comienza a publicar tus vacantes gratis,solo debes crear una cuenta",
+      mensajes: req.flash(),
+    });
+    return;
+  }
 
-        res.render('crear-cuenta',{
-            nombrePagina: 'Crea tu cuenta En devJobs',
-            tagline: 'Comienza a publicar tus vacantes gratis,solo debes crear una cuenta',
-            mensajes: req.flash()     
-        });
-        return;
-         
-    }
+  //si toda la validacion es correcta
+  next();
+};
 
-    //si toda la validacion es correcta
-    next();
-}
+exports.crearUsuario = async (req, res, next) => {
+  //crear el usuario
+  const usuario = new Usuarios(req.body);
 
-exports.crearUsuario= async (req,res,next)=>{
-    //crear el usuario
-    const usuario = new Usuarios(req.body);
-
-    const nuevoUsuario= await usuario.save();
-    if(!nuevoUsuario) return next();
-
-    res.redirect('/iniciar-sesion');
-
-}
-
- 
+  try {
+    await usuario.save();
+    res.redirect("/iniciar-sesion");
+  } catch (error) {
+    req.flash("error", error);
+    res.redirect("/crear-cuenta");
+  }
+};
--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,40 +1,45 @@
-const mongoose= require('mongoose')
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-const bcrypt = require('bcrypt')
-
+const bcrypt = require("bcrypt");
 
 const usuariosSchema = new mongoose.Schema({
-    email:{
-        type: String,
-        unique: true,
-        lowercase:true,
-        trim:true,
-    },
-    nombre:{
-        type: String,
-        requerid: true,
-
-    },
-    password:{
-        type: String,
-        requerid:true,
-        trim:true
-    },
-    token: String,
-    expira:Date
-})
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  nombre: {
+    type: String,
+    requerid: true,
+  },
+  password: {
+    type: String,
+    requerid: true,
+    trim: true,
+  },
+  token: String,
+  expira: Date,
+});
 
 //Metodo para hashear los passwords
-usuariosSchema.pre('save', async function(next){
-    // si el password ya esta hasheado no hacemos nada
-    if(!this.isModified('password')){
-       return next(); 
-    }
-    //si no está hasheado
-    const hash = await bcrypt.hash(this.password, 12);
-    this.password= hash;
-    next();
-})
+usuariosSchema.pre("save", async function (next) {
+  // si el password ya esta hasheado no hacemos nada
+  if (!this.isModified("password")) {
+    return next();
+  }
+  //si no está hasheado
+  const hash = await bcrypt.hash(this.password, 12);
+  this.password = hash;
+  next();
+});
 
+usuariosSchema.post("save", function (error, doc, next) {
+  if (error.name === "MongoError" && error.code === 11000) {
+    next("Ese correo ya está registrado");
+  } else {
+    next(error);
+  }
+});
 
-module.exports = mongoose.model('Usuarios', usuariosSchema)
+module.exports = mongoose.model("Usuarios", usuariosSchema);
